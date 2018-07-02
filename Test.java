d6423694ce0ff2bd965830ea1a532318f9befef7
qingyun <qingyun@wangruixia.local>
2018-07-02 10:33:49 +0800

    修改了Test新建了Student

--- Test.java
+++ Test.java
@@ -3,5 +3,8 @@ public class Tast {
 	public static void main(String[] args) {
 		System.out.println("Hello, World");
 		System.out.println("喝酒断片");
+		System.out.println("你好");
+		System.out.println("jal");
+		System.out.println("jal");
 	}
 }
